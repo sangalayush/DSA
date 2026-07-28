fc89a73e81ac759d91c36cc82f49754349a71d29
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-07-28 19:10:18 +0530

    Implement quicksort algorithm in C++

---
+++ quick sort.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int func(std::vector<int> &arr,int low,int high)
+{
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while(i<j)
+    {
+        while(arr[i]<=pivot && i<=high-1)
+        {
+            i++;
+        }
+        while(arr[j]>pivot && j>=low+1)
+        {
+            j--;
+        }
+        if(i<j)
+        {
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    int temp1=arr[j];
+    arr[j]=arr[low];
+    arr[low]=temp1;
+    return j;
+}
+void qs(std::vector<int> &arr,int low,int high)
+{
+    if(low<high)
+    {
+        int partition=func(arr,low,high);
+        qs(arr,low,partition-1);
+        qs(arr,partition+1,high);
+    }
+}
+
+int main(){
+    std::vector<int> arr={7,5,3,6,4} ;
+    int low=0, high=arr.size()-1;
+    qs(arr,low,high);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
