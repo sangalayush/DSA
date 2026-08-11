3466f9e6edc1bf3f5bebabd199447c28a1781adf
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-08-11 17:41:10 +0530

    Create Move All zeroes to right.cpp

---
+++ Move All zeroes to right.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+//brute-force approach
+void movezeroes(vector<int> &nums){
+    vector<int>temp;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]!=0){
+            temp.push_back(nums[i]);    //store all non-zero elements in temp
+        }
+    }
+    int non_zero= temp.size();      
+    for(int i=0;i<non_zero;i++){    //if 7 non_zero elements then store till non_zero position
+        nums[i]=temp[i];    // store all temp elements at starting of  nums..
+    }
+    for(int i=non_zero;i<nums.size();i++){
+        nums[i]=0;      //from non_zero position put all elements as zero
+    }
+}
+//T.C.--> O(N)+O(x)->x non_zero elements+ O(N-x)
+//total->O(2N)
+//S.C.->O(x)->O(N)-> if all elements are non_zero in nums array.
+int main(){
+    vector<int> nums={1,0,3,0,3,4,0,0,2,0,6};
+    movezeroes(nums);
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
