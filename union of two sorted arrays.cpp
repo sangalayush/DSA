15bf3e94754c9ba8d137edf0a50a66fb2f4120b9
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-08-14 18:33:54 +0530

    Update union of two sorted arrays.cpp

--- union of two sorted arrays.cpp
+++ union of two sorted arrays.cpp
@@ -30,3 +30,41 @@ int main(){
     }
     return 0;
 }
+//optimal approach
+class Solution {
+public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        int i=0;
+        int j=0;
+        vector<int> result;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                if(result.size()==0 ||result.back()!=nums1[i]){
+                    result.push_back(nums1[i]);
+                }
+                i++;
+            }
+            else{
+                if(result.size()==0 ||result.back()!=nums2[j]){
+                    result.push_back(nums2[j]);
+                }
+                j++;
+            }      
+        }
+        while(j<nums2.size()){
+            if(result.size()==0 || result.back()!=nums2[j]){
+                result.push_back(nums2[j]);
+            }
+            j++;
+        }
+        while(i<nums1.size()){
+            if(result.size()==0 || result.back()!=nums1[i]){
+                result.push_back(nums1[i]);
+            }
+            i++;
+        }
+        return result;
+    }
+};
+//T.C.->O(n1+n2)
+//S.C.->O(n1+n2)->required to retuurn the Union Array.
