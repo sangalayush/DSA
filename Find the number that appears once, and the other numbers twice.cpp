00f34f71f4a02eb4bcbbda09198f4156b99c0ae7
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-08-20 22:21:37 +0530

    Update Find the number that appears once, and the other numbers twice.cpp

--- Find the number that appears once, and the other numbers twice.cpp
+++ Find the number that appears once, and the other numbers twice.cpp
@@ -17,6 +17,28 @@ int appear_once(vector<int> &nums){
 }
 //T.C.->O(N*N)
 //S.C.->O(1)
+
+
+//------------------------------------------------------------------------------------------
+
+//Better approach
+int maxi=0;
+int appear_once(vector<int> &nums){
+    for(int i=0;i<nums.size();i++){
+        maxi=max(nums[i],maxi);
+    }
+    vector<int> hash(maxi+1);
+    for(int i=0;i<nums.size();i++){
+        hash[nums[i]]++;
+    }
+    for(int i=0;i<hash.size();i++){
+        if(hash[i]==1)
+            return i;
+    }
+    return -1;
+}
+//T.C.->O(N)
+//S.C.->O(N)
 int main(){
     vector<int> nums={2,2,1,1,3,5,5,4,4};
     int ans= appear_once(nums);
