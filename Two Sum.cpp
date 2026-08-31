e5a26cd14bf1b73e216c5385fa6bb36b0c45affa
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-08-31 17:42:02 +0530

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -7,9 +7,11 @@ vector<int> twosum(vector<int> &nums, int target){
         for(int j=i+1;j<nums.size();j++){
             if(nums[i]+nums[j]==target)
                 return {i,j};
+                //Or we can return YES
         }
     }
     return {-1,-1};
+    //Or we can return NO
 }
 //T.C.->O(N*N)
 //.S.C.->O(1)
