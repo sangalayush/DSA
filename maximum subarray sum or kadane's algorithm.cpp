4cec421e3bedbd1f59a03c1e53fe91df8acc152e
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-10 15:20:25 +0530

    Create maximum subarray sum or kadane's algorithm.cpp

---
+++ maximum subarray sum or kadane's algorithm.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//brut-force approach
+int maximumsubarraysum(vector<int> nums){
+    int maxi= INT_MIN;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i;j<nums.size();j++){
+            int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=nums[k];
+            }
+            maxi=max(sum,maxi);
+        }
+    }
+    return maxi;
+}
+//T.C.->O(N^3)
+//S.C.->O(1)
+int main()
+{
+    vector<int> nums={-2, -3, -7, -2, -10, -4};
+    cout<<maximumsubarraysum(nums);
+}
