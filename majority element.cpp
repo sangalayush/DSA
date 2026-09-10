162f76f50698b3e052f00559d22b4e7082828e5a
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-10 13:15:26 +0530

    Update majority element.cpp

--- majority element.cpp
+++ majority element.cpp
@@ -38,6 +38,32 @@ int majorityelement(vector<int> nums){
 }
 //T.C.->O(N)
 //S.C.->O(N)
+//--------------------------------------------------------------------------
+//Optiaml  approach->using moore's voting algorithm
+int majorityelement(vector<int> nums){
+    int cnt=0, ele=0;
+    for(int i=0;i<nums.size();i++){
+        if(cnt==0)
+            cnt=1;
+            ele=nums[i];
+        if(nums[i]==ele){
+            cnt++;
+        }
+        else
+            cnt--;
+    }
+    //check if it is actually the majorityelement
+    int count=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==ele)
+            count++;
+    }
+    if(count>nums.size()/2)
+        return ele;
+    return -1;
+}
+//T.C.->O(N)
+//S.C.->O(1)
 int main()
 {
     vector<int> nums={2,2,1,5,5,1,5,5,2,5,5,5};
