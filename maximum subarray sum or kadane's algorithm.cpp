410ae29e5fa37236530424b14bd33ea9f2cfa342
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-11 11:33:01 +0530

    Update maximum subarray sum or kadane's algorithm.cpp

--- maximum subarray sum or kadane's algorithm.cpp
+++ maximum subarray sum or kadane's algorithm.cpp
@@ -16,6 +16,23 @@ int maximumsubarraysum(vector<int> nums){
 }
 //T.C.->O(N^3)
 //S.C.->O(1)
+//---------------------------------------------------------------
+//Better approach
+int maximumsubarraysum(vector<int> nums){
+    int maxi= INT_MIN;
+    for(int i=0;i<nums.size();i++){
+        int sum=0;
+        for(int j=i;j<nums.size();j++){
+            sum+= nums[j];
+            maxi= max(sum,maxi);
+        }
+    }
+    return maxi;
+}
+//T.C.->O(N^2)
+//S.C.->O(1)
+//----------------------------------------------------------------
+
 int main()
 {
     vector<int> nums={-2, -3, -7, -2, -10, -4};
