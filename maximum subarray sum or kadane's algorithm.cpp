43e13543345bab535c9d7a059c4e6cfdbca87dff
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-11 11:57:57 +0530

    Update maximum subarray sum or kadane's algorithm.cpp

--- maximum subarray sum or kadane's algorithm.cpp
+++ maximum subarray sum or kadane's algorithm.cpp
@@ -51,3 +51,38 @@ int main()
     vector<int> nums={-2, -3, -7, -2, -10, -4};
     cout<<maximumsubarraysum(nums);
 }
+//Print the subaaray
+#include<bits/stdc++.h>
+using namespace std;
+//Optimal approach->using kadane's algorithm
+int maximumsubarraysum(vector<int> nums){
+    int maxi= INT_MIN;
+    int sum=0;      //initialize sum as 0
+    int ansstart=-1, ansend=-1;
+    int start=0;
+    for(int i=0;i<nums.size();i++){
+        if(sum==0)
+            int start=i;
+       sum+= nums[i];               // increment the sum by nums[i] value
+       if(sum>maxi){
+        maxi=sum;               //update the maxi if sum>previous maxi
+        ansstart= start;
+        ansend=i;
+       }
+       if(sum<0)                    //if sum<0 leave the sum
+        sum=0;                      //and put sum to 0 bcs adding -ve values to sum dec. the sum value
+    }
+    //print the subarray that has the maximum sum
+    for(int i=ansstart;i<=ansend;i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    return maxi;
+}
+//T.C.->O(N)
+//S.C.->O(1)
+int main()
+{
+    vector<int> nums={2, 3, 5, -2, 7, -4};
+    cout<<maximumsubarraysum(nums);
+}
