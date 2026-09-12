cf0548a6f070f5ad44767a777e12307e3e2816d3
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-12 13:50:07 +0530

    Create Buy stock & sell.cpp

---
+++ Buy stock & sell.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+//brute-force
+int buysellstock(vector<int> nums){
+    int maxprofit=0, profit=0;
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            profit= nums[j]-nums[i];
+            maxprofit= max(maxprofit,profit);
+        }
+    }
+    return maxprofit;
+}
+//T.C.->O(N*N)
+//S.C.->O(1)
+int main()
+{
+    vector<int> nums={7,1,5,3,6,4};
+    cout<<buysellstock(nums);
+    return 0;
+}
