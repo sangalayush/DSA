2f9d869d539d1f8cdbfc31270e31336f997bb4ca
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-12 13:56:50 +0530

    Update Buy stock & sell.cpp

--- Buy stock & sell.cpp
+++ Buy stock & sell.cpp
@@ -14,6 +14,19 @@ int buysellstock(vector<int> nums){
 }
 //T.C.->O(N*N)
 //S.C.->O(1)
+//----------------------------------------------------------------
+//optimal
+int buysellstock(vector<int> nums){
+    int mini=nums[0], maxprofit=0, profit=0;
+    for(int i=1;i<nums.size();i++){
+        profit= nums[i]-mini;
+        maxprofit= max(maxprofit, profit);
+        mini= min(mini,nums[i]);
+    }
+    return maxprofit;
+}
+//T.C.->O(N)
+//S.C.->O(1)
 int main()
 {
     vector<int> nums={7,1,5,3,6,4};
