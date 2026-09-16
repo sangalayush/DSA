6c8df196630c3d5062a5eff7151ef6446f7f4755
sangalayush <144334548+sangalayush@users.noreply.github.com>
2026-09-16 22:46:50 +0530

    Update Rearrange Array Elements by Sign.cpp

--- Rearrange Array Elements by Sign.cpp
+++ Rearrange Array Elements by Sign.cpp
@@ -58,5 +58,53 @@ int main() {
     }
     return 0;
 }
+//--------------------------------------------------------------------------------------
 //for unequal no. of + & - elements
 //i.e. if pos>neg or neg>pos
+#include <iostream>
+#include<vector>
+using namespace std;
+    void rearrangeelements(vector<int> &nums){
+        vector<int> pos;
+        vector<int> neg;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0)
+                neg.push_back(nums[i]);
+            else
+                pos.push_back(nums[i]);
+        }
+        int p= pos.size();
+        int n= neg.size();
+        if(p>n){
+            for(int i=0;i<n;i++){
+                nums[2*i]= pos[i];
+                nums[2*i+1]= neg[i];
+            }
+            int index= n*2;
+            for(int i=n;i<p;i++){
+                nums[index]= pos[i];
+                index++;
+            }
+        }
+        else{
+            for(int i=0;i<p;i++){
+                nums[2*i]= pos[i];
+                nums[2*i+1]= neg[i];
+            }
+            int index= p*2;
+            for(int i=p;i<n;i++){
+                nums[index]= neg[i];
+                index++;
+            }
+        }
+}
+//T.C.->O(N+N)=O(2N)
+//S.C.->O(N)
+int main() {
+    vector<int> nums={1,2,3,-4,5,6,-9,-10};
+    rearrangeelements(nums);
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
